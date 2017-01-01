ff50bb881f75a3c35db1b91f1acf3060093213f7
Bhargav R. Patel <ziikutv@gmail.com>
2017-01-01 15:42:05 -0500

    Add internet code for darwin

--- app/js/common.js
+++ app/js/common.js
@@ -5,8 +5,10 @@
 	Functions which are imported in HTML file(s)
 
 */
-var cmd=require('node-cmd');
+const cmd = require('node-cmd');
 const {ipcRenderer} = require('electron');
+const platform = require('os').platform();
+
 
 function closeApp() {
 	ipcRenderer.send('state-messages', 'close');
@@ -32,4 +34,31 @@ function sessionEndNotification() {
 	notif.onclick = function () {
 	  ipcRenderer.send('state-messages', 'focus')
 	}
+}
+
+function runInternetCommands(enable) {
+	if (platform === 'darwin') {
+		if (enable) {
+			commands = ['networksetup -setairportpower en0 on']
+		} else {
+			commands = ['networksetup -setairportpower en0 off']
+		}
+	} else if (platform === 'win32') {
+		// TODO: Add command for Windows and test on PC.
+		return;
+	} else {
+		// TODO: Research how to run command for Linux
+		return;
+	}
+	commands.forEach(function (command, index, array) {
+	  cmd.run(command);
+	});
+}
+
+function enableInternet() {
+	runInternetCommands(true);
+}
+
+function disableInternet() {
+	runInternetCommands(false);
 }
